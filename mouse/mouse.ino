0873c5038a01b71a990f059f26471f35678755a8
robgle <robby.gleiser@gmail.com>
2018-04-05 22:07:48 -0700

    PID values

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -2,6 +2,7 @@
 
 #include <VL6180X.h>
 #include <Wire.h>
+#include <PID_v1.h>
 
 // Invert encoder directions if needed
 const boolean INVERT_ENCODER_LEFT = true;
@@ -21,9 +22,26 @@ float left_dist;
 float right_dist;
 float center_dist;
 
+// PID control
+double v, u_lin, u_ang, error, v_sp;
+double zero = 0;
+double P = .242;
+double I = 0.009;
+double D = 0.0012;
+PID linPID(&v, &u_lin, &v_sp, P, I, D, DIRECT);
+PID angPID(&error, &u_ang, &zero, P, I, D, DIRECT);
+
+
 void setup() {
   Serial.begin(9600);
   hardwareSetup();
+  v_sp = 100;
+  error = 0;
+  angPID.SetOutputLimits(-1.0, 1.0);
+  angPID.SetSampleTime(10);
+  linPID.SetOutputLimits(0, 1.0);
+  linPID.SetMode(AUTOMATIC);
+  angPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -33,93 +51,38 @@ void loop() {
   center_dist = getDistanceCenter();
 
   velocity_linear = getLinearVelocity();
-  velocity_angular = getAngularVelocity();
-  
+  velocity_angular = getAngularVelocity(); // > 0 when right > left
+
   ////////////////////////////////////
   // Your changes should start here //
   ////////////////////////////////////
+  error = velocity_angular;
+  v = velocity_linear;
 
-  float left_power = 0.3;
-  float right_power = 0.3;
+  linPID.Compute();
+  angPID.Compute();
 
-  float u_current = u(sr_error),
-        pleft = left_power + u_current,
-        pright = right_power - u_current;
-        
-  //applyPowerLeft(pleft);
-  //applyPowerRight(pright);
-
-  applyPowerLeft(pleft);
-  applyPowerRight(pright);
+  applyPowerLeft(.5 - u_ang);
+  applyPowerRight(.5 + u_ang);
 
   // Print debug info every 500 loops
   if (count % 500 == 0) {
-    Serial.print(" vlin");
-    Serial.print(velocity_linear / 100.0);
-    Serial.print(" vang");
-    Serial.print(velocity_angular);
-    Serial.print(" ldist");
-    Serial.print(left_dist);
-    Serial.print(" cdist");
-    Serial.print(center_dist);
-    Serial.print(" rdist");
-    Serial.print(right_dist);
-    Serial.print(" ucurr");
-    Serial.print(u_current);
-    Serial.print(" pl");
-    Serial.print(pleft);
-    Serial.print(" pr");
-    Serial.print(pright);
-    
+    // Serial.print(" V: ");
+    // Serial.print(velocity_linear / 100.0);
+    Serial.print(" V_A: ");
+    Serial.print(error);
+    // Serial.print(" LEFT: ");
+    // Serial.print(left_dist);
+    // Serial.print(" CENTER: ");
+    // Serial.print(center_dist);
+    // Serial.print(" RIGHT: ");
+    // Serial.print(right_dist);
+    Serial.print(" CRCTION: ");
+    Serial.print(u_ang);
     Serial.println();
-
   }
   count++;
 
   checkEncodersZeroVelocity();
   updateDistanceSensors();
-  setlight();
-}
-
-void setlight()
-{
-  if(sr_error() < 0)
-  {
-    digitalWrite(PIN_LED1, HIGH);
-    digitalWrite(PIN_LED2, LOW);
-  }
-  else
-  {
-    digitalWrite(PIN_LED1, LOW);
-    digitalWrite(PIN_LED2, HIGH);
-  }
-
-}
-//u(t)
-//return:
-//- : add power to right engine, turn left
-//+ : add power to left engine, turn right
-float u(float (*err_f)())
-{
-  static float current_correction_rate = 0.0;
-  
-  static unsigned long last_fix = 0;
-  unsigned long current_time;
-  
-  current_time = millis();
-  if(current_time - last_fix > 100){
-    current_correction_rate += sr_error() * 0.005;
-    last_fix = current_time;
-  }
-
-
-  return current_correction_rate;
-}
-
-//return:
-//-: need to go left
-//+: need to go right
-float sr_error()
-{
-  return velocity_angular+0.15;
 }
